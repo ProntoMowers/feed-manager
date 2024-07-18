b551e26c2262226ccdade769404ba48f16ad0706
Anezeres <alvarez.julian@correounivalle.edu.co>
2024-07-18 13:42:02 -0500

    Se hacen cambios para ejecutar en develop

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.use(appRouter);
 
 const {startCronJob} = require("./helpers/queue.js")
 // Server is listening
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
   //startCronJob();
