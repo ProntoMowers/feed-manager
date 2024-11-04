1bea409517fb35f149860821994be1ed8a3ef309
Anezeres <alvarez.julian@correounivalle.edu.co>
2024-11-04 15:07:45 -0500

    Se hace una modificación para que nos ejecuten los crosn

--- cron-task.js
+++ cron-task.js
@@ -1,15 +1,14 @@
 // cron-task.js
 
-
 const { fetchOneFromTable, updateFeed } = require("./databases/CRUD");
-const { createWebhookToCreateProduct, createWebhookToUpdateProduct, fetchWebHooks } = require("./api/webHooksBigCommerceApi")
-const { listAllProducts } = require("./api/googleMerchantAPI")
-const { countProductsByAvailability, countTotalProducts } = require("./api/productsBigCommerceApi")
-const { listAllProductIds } = require("./api/googleMerchantAPI")
-const {manageProductProcessingFeed, countPagesFeed, manageProductSync, countPagesNew} =require("./api/checkProductsFeeds")
-const {buildQueryUrl} =require("./helpers/helpers")
-const { checkAndResetCheckpoints } = require("./databases/CRUD")
-// Obtener el feedId de las variables de entorno
+const { createWebhookToCreateProduct, createWebhookToUpdateProduct, fetchWebHooks } = require("./api/webHooksBigCommerceApi");
+const { listAllProducts } = require("./api/googleMerchantAPI");
+const { countProductsByAvailability, countTotalProducts } = require("./api/productsBigCommerceApi");
+const { listAllProductIds } = require("./api/googleMerchantAPI");
+const { manageProductProcessingFeed, countPagesFeed, manageProductSync, countPagesNew } = require("./api/checkProductsFeeds");
+const { buildQueryUrl } = require("./helpers/helpers");
+const { checkAndResetCheckpoints } = require("./databases/CRUD");
+
 const args = process.argv.slice(2);
 const feedId = args[0];
 
@@ -18,106 +17,78 @@ if (!feedId) {
     process.exit(1);
 }
 
-// Función principal de sincronización
 async function synchronizeFeedCron(feedId) {
-
-    console.log("El cron funciona correctamente y el Feed id es: ", feedId)
+    console.log("El cron funciona correctamente y el Feed id es: ", feedId);
     const currentDateTime = new Date();
     console.log("Fecha y hora de ejecución:", currentDateTime.toISOString());
 
     try {
         const feed = await fetchOneFromTable('feeds', feedId, 'feed_id');
         if (feed) {
-            const storeHash = feed.store_hash;
-            const accessToken = feed.x_auth_token;
-            const privateKey = feed.private_key;
-            const merchantId = feed.client_id;
-            const formula = feed.formulas;
+            const { store_hash, x_auth_token, private_key, client_id, formulas, client_email, domain } = feed;
 
-            const baseUrl = `https://api.bigcommerce.com/stores/${storeHash}/v3/catalog/products`;
-            const url = await buildQueryUrl(baseUrl, formula);
-
-            
-            const config = {
-                accessToken: accessToken,
-                storeHash: storeHash,
-                client_email: feed.client_email,
-                private_key: privateKey,
-                merchantId: merchantId,
-                domain: feed.domain,
-                apiInfo: url
-            };
+            const baseUrl = `https://api.bigcommerce.com/stores/${store_hash}/v3/catalog/products`;
+            const url = await buildQueryUrl(baseUrl, formulas);
 
             const configBase = {
-                accessToken: accessToken,
-                storeHash: storeHash,
-                client_email: feed.client_email,
-                private_key: privateKey,
-                merchantId: merchantId,
-                domain: feed.domain,
+                accessToken: x_auth_token,
+                storeHash: store_hash,
+                client_email,
+                private_key,
+                merchantId: client_id,
+                domain,
             };
 
-            // Verificar y reiniciar checkpoints si es necesario
             await checkAndResetCheckpoints(feedId);
 
-            console.log("Config: ", config);
+            for (const urlActual of url.url) {
+                const config = {
+                    ...configBase,
+                    apiInfo: { url: urlActual, customFields: url.customFields }
+                };
 
-            // Ejecutar las operaciones asíncronas en segundo plano
-            setImmediate(async () => {
                 try {
-                    for (const urlActual of url.url) {
-                        const config = {
-                            ...configBase,
-                            apiInfo: { url: urlActual, customFields:url.customFields }
-                        };
-                        console.log("Config info: ", config);
-                        
-                        
-                        const productsSKUs = await listAllProductIds(config);
-                        console.log("Listos los ID's");
-                        const conteoPages = await countPagesNew(config);
-                        console.log("Conteo: ", conteoPages);
-                        const conteoByTipo = await manageProductProcessingFeed(config, feedId, conteoPages, urlActual);
-                        console.log("Conteo por tipo: ", conteoByTipo);
-                    }
-                    //const conteoByTipo = await manageProductProcessingFeed(config, conteoPages);
+                    const productsSKUs = await listAllProductIds(config);
+                    console.log("Listos los ID's");
 
+                    const conteoPages = await countPagesNew(config);
+                    console.log("Conteo: ", conteoPages);
 
-                    const WebHooks = await fetchWebHooks(config);
-
-                    if (WebHooks.data.length == 0) {
-                        await createWebhookToCreateProduct(config, feedId);
-                        await createWebhookToUpdateProduct(config, feedId);
-                    }
-
-                    // Ejecutar las operaciones de conteo en paralelo
-                    const [totalProductsGM, totalProductsBC, preorderProducts] = await Promise.all([
-                        listAllProducts(config),
-                        countTotalProducts(config),
-                        countProductsByAvailability(config, "preorder")
-                    ]);
-
-                    const updateData = {
-                        total_products_bc: totalProductsBC,
-                        active_products_gm: totalProductsGM,
-                        preorder_products: preorderProducts
-                    };
-
-                    await updateFeed(feedId, updateData);
-
-                    console.log("Sincronización completada y feed actualizado");
+                    await manageProductProcessingFeed(config, feedId, conteoPages, urlActual);
                 } catch (error) {
-                    console.error('Error durante la sincronización en segundo plano:', error);
+                    console.error('Error en la sincronización de productos:', error);
                 }
-            });
+            }
 
+            const webhooks = await fetchWebHooks(configBase);
+            if (webhooks.data.length === 0) {
+                await createWebhookToCreateProduct(configBase, feedId);
+                await createWebhookToUpdateProduct(configBase, feedId);
+            }
+
+            const [totalProductsGM, totalProductsBC, preorderProducts] = await Promise.all([
+                listAllProducts(configBase),
+                countTotalProducts(configBase),
+                countProductsByAvailability(configBase, "preorder")
+            ]);
+
+            const updateData = {
+                total_products_bc: totalProductsBC,
+                active_products_gm: totalProductsGM,
+                preorder_products: preorderProducts
+            };
+
+            await updateFeed(feedId, updateData);
+            console.log("Sincronización completada y feed actualizado");
         } else {
             console.error('Feed no encontrado');
         }
     } catch (error) {
         console.error('Error al obtener el feed:', error);
+    } finally {
+        // Finalizar el proceso después de completar la tarea
+        process.exit(0);
     }
-
 }
 
 // Ejecutar la función de sincronización
